00e069b2902ed496e81f8c6c627502b56a0dfed5
Scott Blumenthal <scott.blumenthal@gmail.com>
2024-05-28 17:15:21 -0400

    remove unnecessary date formatting code

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -19,14 +19,7 @@ export const Post = (
   //       .join(", ")}
   //   </footer>
   // ) : null;
-  let parsedDate = date;
-  try {
-    // if it looks like a time was not specified, shift from GMT to EST
-    const ESTDate = date.replace("T00:00:00.000Z", "T05:00:00.000Z");
-    parsedDate = format(parseISO(ESTDate), "MMMM d, y");
-  } catch (e) {
-    // there is either NO date, or date is already a formatted string.
-  }
+
   return (
     <div className={Styles.post}>
       {title ? <h3>{linkTitle ? <a href={slug}>{title}</a> : title}</h3> : null}
