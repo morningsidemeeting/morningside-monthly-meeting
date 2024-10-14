8d16b150ce6b8cb0c8d626e5a6b0138daf926c2a
Scott Blumenthal <scott.blumenthal@gmail.com>
2024-10-14 09:24:03 -0400

    copy edits from Robert Renwick to Caregivers page.

--- src/pages/first-day-caregivers/index.js
+++ src/pages/first-day-caregivers/index.js
@@ -11,17 +11,16 @@ export default function CommitteePage() {
           During Meeting for Worship at least two caregivers, usually one paid
           and one volunteer, are available to be with children who may attend.
           They will also be available during Meeting for Worship with a Concern
-          for Business if requested.{" "}
+          for Business if requested.
         </li>
         <li>
           Caregivers and parents work together to consider policies, materials,
           and activities.
         </li>
         <li>
-          We seek to offer a place of love, bringing our testimonies to life and
-          laughter while allowing each of the children to be themselves, to
-          offer a place where everyone there learns through respect, love, and
-          fun.
+          We seek to offer a place bringing our testimonies to life and laughter
+          while allowing the children to be themselves, a place where everyone
+          there learns through respect, love, and fun.
         </li>
       </ul>
     </Committee>
